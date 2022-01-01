7924394cbd85f133ef5e9f07b76de25c24823bf3
Amina <aminajusufovic4@gmail.com>
2022-01-01 23:24:14 +0100

    site

---
+++ js/vezba.js
@@ -0,0 +1,61 @@
+// a = window.prompt("Unesite broj");
+// if (a == 0) {
+//   console.log("Uneti broj je jednak nuli");
+// }
+// if (a % 2 == 0) {
+//   console.log("Uneti broj je paran");
+// }
+// if (a % 2 == 1) {
+//   console.log("Uneti broj je neparan");
+// }
+
+// a = window.prompt("Unesite prvi broj");
+// b = window.prompt("Unesite drugi broj");
+// c = window.prompt("Unesite treci broj");
+// let d = Math.max(a, b, c);
+// console.log(d);
+
+// a = window.prompt("Unesite prvi broj");
+// b = window.prompt("Unesite drugi broj");
+// c = window.prompt("Unesite treci broj");
+// var brojevi = [a, b, c];
+// brojevi.sort();
+// console.log(brojevi);
+
+// a = window.prompt("Unesite prvi broj");
+// b = window.prompt("Unesite drugi broj");
+// c = window.prompt("Unesite treci broj");
+// if (a + b > c || a + c > b || b + c > a) {
+//   console.log("moguce je napraviti trougao od unetih vrednosti");
+// } else {
+//   console.log("Nije moguce je napraviti trougao");
+// }
+
+// a = prompt("Unesite broj od 1 do 100");
+// switch (a) {
+//   case "1":
+//     console.log("Broj je low");
+//     break;
+//   default:
+//     console.log("Vas unos je pogresan pokusajte ponovo");}
+
+// a = -5;
+// b = -2;
+// c = -6;
+// d = 0;
+// e = -1;
+// if (a > b && a > c && a > d && a > e) {
+//   console.log(a);
+// }
+// if (b > a && b > c && b > d && b > e) {
+//   console.log(b);
+// }
+// if (c > b && c > a && c > d && c > e) {
+//   console.log(c);
+// }
+// if (d > b && d > c && d > a && d > e) {
+//   console.log(d);
+// }
+// if (e > b && e > c && e > a && e > a) {
+//   console.log(e);
+// }
