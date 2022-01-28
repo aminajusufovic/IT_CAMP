3dc123493d704cba40fb85c016afa63c22fd5520
Amina <aminajusufovic4@gmail.com>
2022-01-28 14:56:25 +0100

    mmm

--- js/vezba.js
+++ js/vezba.js
@@ -459,6 +459,58 @@
 
 // ime(unos);
 
-function twiceAsOld(dadYearsOld, sonYearsOld) {
-  return Math.abs(dadYearsOld - 2 * sonYearsOld);
+// function twiceAsOld(dadYearsOld, sonYearsOld) {
+//   return Math.abs(dadYearsOld - 2 * sonYearsOld);
+// }
+
+// function broj(niz) {
+//   min1 = niz[0];
+//   min2 = niz[0];
+//   for (el of niz) {
+//     if (min1 > el) {
+//       min1 = el;
+//     } else if (min2 > el && min1 < el) {
+//       min2 = el;
+//     }
+//   }
+//   console.log("Najmanji je", min1);
+//   console.log("Drugi Najmanji je", min2);
+// }
+// broj([2, 3, 4, 7, 9]);
+
+// function nbroj(arr) {
+//   min1 = arr[0];
+//   arr = arr;
+//   for (el of arr) {
+//     if (el > min1) {
+//       min1 = el;
+//     }
+//   }
+//   console.log("Najduzi broj je", min1);
+// }
+
+// nbroj([1, 21, 223, -5343]);
+
+// function nbroj(arr) {
+//   min1 = arr[0];
+//   for (el of arr) {
+//     if (Math.abs(min1) <= Math.abs(el)) {
+//       min1 = el;
+//     }
+//   }
+//   console.log("Najduzi broj je", min1);
+// }
+
+// nbroj([1, 21, 223, -5343]);
+
+// nule([3, 0, 24, 54, 0, 123, 0, 32, 63]);
+
+function prosek(niz) {
+  sum = 0;
+  for (el of niz) {
+    sum += el;
+  }
+  console.log("prosek je", sum / niz.length);
 }
+
+prosek([3, 24, 54, 123, 32, 63]);
