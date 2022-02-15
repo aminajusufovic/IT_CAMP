2ccd61fafe0e5887a9d6cf14bfb42dc929b6088b
Amina <aminajusufovic4@gmail.com>
2022-02-15 18:12:28 +0100

    commit

--- js/vezba.js
+++ js/vezba.js
@@ -719,97 +719,121 @@
 // console.log(o1);
 // console.log(o2);
 
-class Book {
-  constructor(title = "", genre, author) {
-    this.title = title;
-    this.genre = genre;
-    this.author = author;
-    this.read = false;
-    this.read_date = null;
-  }
+// class Book {
+//   constructor(title = "", genre, author) {
+//     this.title = title;
+//     this.genre = genre;
+//     this.author = author;
+//     this.read = false;
+//     this.read_date = null;
+//   }
+// }
+
+// class BookLists {
+//   constructor() {
+//     this.bookShelf = [];
+//     this.booksRead = 0;
+//     this.notBooksRead = this.bookShelf.length - this.booksRead;
+//     this.nextBook = null;
+//     this.currBook = null;
+//     this.lastBook = null;
+//   }
+
+//   add(book) {
+//     if (book instanceof Book) {
+//       debugger;
+//       this.bookShelf.push(book);
+
+//       for (let i = 0; i < this.bookShelf.length; i++) {
+//         if (!this.bookShelf[i].read) {
+//           this.currBook = this.bookShelf[i];
+//           if (this.bookShelf[i + 1]) {
+//             this.nextBook = this.bookShelf[i + 1];
+//           } else {
+//             this.nextBook = null;
+//           }
+//           break;
+//         }
+//       }
+//     } else {
+//       console.log("Invalid argument type");
+//     }
+//   }
+
+//   finishCurrentBook() {
+//     for (let i = 0; i < this.bookShelf.length; i++) {
+//       const book = this.bookShelf[i];
+//       if (book.title === this.currBook.title) {
+//         this.bookShelf[i].read = true;
+//         this.bookShelf[i].read_date = new Date();
+//         this.lastBook = this.currBook;
+//         this.currBook = this.nextBook;
+//         if (this.bookShelf[i + 1]) {
+//           this.nextBook = this.bookShelf[i + 2];
+//         }
+//         break;
+//       }
+//     }
+//   }
+// }
+
+// b1 = new Book("HP1", "magic", "JKR");
+// b2 = new Book("HP2", "magic", "JKR");
+// b3 = new Book("HP3", "magic", "JKR");
+// b4 = new Book("HP4", "magic", "JKR");
+// b5 = new Book("HP5", "magic", "JKR");
+
+// bl = new BookLists();
+
+// bl.add(b1);
+// bl.add(b2);
+// bl.add(b3);
+// bl.add(b4);
+// bl.add(b5);
+
+// console.log(bl.lastBook);
+// console.log(bl.currBook);
+// console.log(bl.nextBook);
+
+// bl.finishCurrentBook();
+
+// console.log("=========================");
+// console.log(bl.lastBook);
+// console.log(bl.currBook);
+// console.log(bl.nextBook);
+
+// bl.finishCurrentBook();
+// bl.finishCurrentBook();
+// bl.finishCurrentBook();
+// bl.finishCurrentBook();
+
+// console.log("=========================");
+// console.log(bl.lastBook);
+// console.log(bl.currBook);
+// console.log(bl.nextBook);
+
+// console.log(bl.bookShelf);
+
+class Macka
+{
+  constructor(tiredness, lonliness, hunger, happiness)
+  {
+    this.tiredness = tiredness;
+    this.lonliness = lonliness;
+    this.hunger = hunger;
+    this.happiness = happiness;
+  },
+ igranje: function()
+{
+  hunger += 5;
+  
+  
+    
+  },
 }
 
-class BookLists {
-  constructor() {
-    this.bookShelf = [];
-    this.booksRead = 0;
-    this.notBooksRead = this.bookShelf.length - this.booksRead;
-    this.nextBook = null;
-    this.currBook = null;
-    this.lastBook = null;
-  }
+m1 = new Macka("");
+console.log(m1);
 
-  add(book) {
-    if (book instanceof Book) {
-      debugger;
-      this.bookShelf.push(book);
-
-      for (let i = 0; i < this.bookShelf.length; i++) {
-        if (!this.bookShelf[i].read) {
-          this.currBook = this.bookShelf[i];
-          if (this.bookShelf[i + 1]) {
-            this.nextBook = this.bookShelf[i + 1];
-          } else {
-            this.nextBook = null;
-          }
-          break;
-        }
-      }
-    } else {
-      console.log("Invalid argument type");
-    }
-  }
-
-  finishCurrentBook() {
-    for (let i = 0; i < this.bookShelf.length; i++) {
-      const book = this.bookShelf[i];
-      if (book.title === this.currBook.title) {
-        this.bookShelf[i].read = true;
-        this.bookShelf[i].read_date = new Date();
-        this.lastBook = this.currBook;
-        this.currBook = this.nextBook;
-        if (this.bookShelf[i + 1]) {
-          this.nextBook = this.bookShelf[i + 2];
-        }
-        break;
-      }
-    }
-  }
-}
-
-b1 = new Book("HP1", "magic", "JKR");
-b2 = new Book("HP2", "magic", "JKR");
-b3 = new Book("HP3", "magic", "JKR");
-b4 = new Book("HP4", "magic", "JKR");
-b5 = new Book("HP5", "magic", "JKR");
-
-bl = new BookLists();
-
-bl.add(b1);
-bl.add(b2);
-bl.add(b3);
-bl.add(b4);
-bl.add(b5);
-
-console.log(bl.lastBook);
-console.log(bl.currBook);
-console.log(bl.nextBook);
-
-bl.finishCurrentBook();
-
-console.log("=========================");
-console.log(bl.lastBook);
-console.log(bl.currBook);
-console.log(bl.nextBook);
-
-bl.finishCurrentBook();
-bl.finishCurrentBook();
-bl.finishCurrentBook();
-bl.finishCurrentBook();
-
-console.log("=========================");
-console.log(bl.lastBook);
-console.log(bl.currBook);
-console.log(bl.nextBook);
-
-console.log(bl.bookShelf);
+m2 = new Macka();
+console.log(m2);
