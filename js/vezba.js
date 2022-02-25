f8c34a9fa34e8050b1c43ca3313663fd68787dd7
Amina <aminajusufovic4@gmail.com>
2022-02-25 13:18:47 +0100

    commit

--- js/vezba.js
+++ js/vezba.js
@@ -8,20 +8,17 @@
 // if (a % 2 == 1) {
 //   console.log("Uneti broj je neparan");
 // }
-
 // a = window.prompt("Unesite prvi broj");
 // b = window.prompt("Unesite drugi broj");
 // c = window.prompt("Unesite treci broj");
 // let d = Math.max(a, b, c);
 // console.log(d);
-
 // a = window.prompt("Unesite prvi broj");
 // b = window.prompt("Unesite drugi broj");
 // c = window.prompt("Unesite treci broj");
 // var brojevi = [a, b, c];
 // brojevi.sort();
 // console.log(brojevi);
-
 // a = window.prompt("Unesite prvi broj");
 // b = window.prompt("Unesite drugi broj");
 // c = window.prompt("Unesite treci broj");
@@ -30,7 +27,6 @@
 // } else {
 //   console.log("Nije moguce je napraviti trougao");
 // }
-
 // a = prompt("Unesite broj od 1 do 100");
 // switch (a) {
 //   case "1":
@@ -38,7 +34,6 @@
 //     break;
 //   default:
 //     console.log("Vas unos je pogresan pokusajte ponovo");}
-
 // a = -5;
 // b = -2;
 // c = -6;
@@ -58,7 +53,6 @@
 // }
 // if (e > b && e > c && e > a && e > a) {
 //   console.log(e
-
 // for (var i = 0; i <= 15; i++) {
 //   if (i == 0) {
 //     console.log(i, "Ovaj broj je jednak nuli");
@@ -68,7 +62,6 @@
 //     console.log(i, "Ovaj broj je neparan");
 //   }
 // }
-
 // var sum = 0;
 // for (var x = 0; x < 1000; x++) {
 //   if (x % 3 === 0 && x % 5 === 0) {
@@ -76,7 +69,6 @@
 //   }
 // }
 // console.log(sum);
-
 // for (var i = 1; i <= 50; i++) {
 //   if (i % 3 === 0 && i % 5 === 0) {
 //     console.log(i, "fizzbuzz");
@@ -88,14 +80,12 @@
 //     console.log(i);
 //   }
 // }
-
 // sum = 1;
 // x = prompt("Unesi broj");
 // for (let i = 1; i <= x; i++) {
 //   sum *= i;
 // }
 // console.log(sum);
-
 // br1 = 1;
 // br2 = 1;
 // sum = 0;
@@ -107,7 +97,6 @@
 //   br2 = sum;
 //   console.log(sum);
 // }
-
 // i = 1;
 // j = 1;
 // var sum;
@@ -117,7 +106,6 @@
 //     sum += "*";
 //   }
 //   console.log(sum);}
-
 // for (let i = 0; i <= 10; i++) {
 //   var red = "";
 //   for (let j = 1; j <= 10; j++) {
@@ -125,13 +113,10 @@
 //   }
 //   console.log(red, "\n");
 // }
-
 // var pozTemp = 0;
 // var negTemp = 0;
-
 // var pozBr = 0;
 // var negBr = 0;
-
 // for (let i = 0; i < 10; i++) {
 //   t = Number(prompt("Unesite temp:"));
 //   if (t > 0) {
@@ -144,26 +129,22 @@
 // }
 // console.log("Srednja pozitivna vrednost je", pozTemp / pozBr);
 // console.log("Srednja pozitivna vrednost je", negTemp / negBr);
-
 // rec = "ratar";
 // if (rec[0] === rec[4] && rec[1] === rec[3]) {
 //   console.log(" rec je palindrom");
 // } else {
 //   console.log("rec nije palindrom");
 // }
-
 // rec = "ratar";
 // for (let i = 0; i < Math.floor(rec.length / 2); i++) {
 //   if (rec[i] !== rec[rec.length - 1 - i]) {
 //     console.log("rec nije palindrom");
 //     break;
 //   }
-
 //   if (i === Math.floor(rec.length / 2) - 1) {
 //     console.log("rec je palindrom");
 //   }
 // }
-
 // let ans = 0;
 // for (let i = 999; i >= 100; i--) {
 //   for (let j = 999; j >= 100; j--) {
@@ -175,9 +156,7 @@
 //     }
 //   }
 // }
-
 // console.log(ans);
-
 // lozinka = prompt("unesite lozinku");
 // while (lozinka < 10) {
 //   lozinka++;
@@ -186,20 +165,16 @@
 //   }
 //   break;
 // }
-
 // moja_lozinka = "sifra";
 // kor_lozinka = prompt("unesite lozinku");
-
 // while (moja_lozinka !== kor_lozinka) {
 //   kor_lozinka = prompt("Pogresna lozinka pokusajte ponovo:");
 // }
 // alert("tacna lozinka");
-
 // kor_unos = prompt("unesite broj u rasponu od 10-20");
 // while (kor_unos <= 20 && kor_unos >= 10) {
 //   console.log("cetitamo uneli ste broju rasponu", kor_unos);
 // }
-
 // x = prompt(
 //   "izaberite jednu od racunskih operacija:    1. (+)    2. (-)    3. (*)    4. (/)     5. (Izlaz iz programa)"
 // );
@@ -235,23 +210,19 @@
 // while ("5" === x) {
 //   break;
 // }
-
 // ucenici = [];
 // brUc = Number(prompt("broj ucenika"));
 // for (let i = 0; i < brUc; i++) {
 //   visUc = Number(prompt("Visina ucenika"));
 //   ucenici.push(visUc);
 // }
-
 // console.log(ucenici);
-
 ////////////////////////////////////////////////
 // min = ucenici;
 // max = ucenici;
 // for (let i = 0; 0 < ucenici.length; i++) {
 //     if(max< ucenici[i])
 // }
-
 // sum = 0;
 // const niz = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // for (let i = 1; i <= niz.length; i++) {
@@ -260,14 +231,11 @@
 //   }
 // }
 // console.log(sum);
-
 // const niz = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // for (let i = 1; i <= niz.length; i++) {
 //   niz[i] = niz[i] * 3;
 // }
-
 // console.log(niz[i]);
-
 // const nizJedan = [];
 // const nizDva = [1, 2, 3, 4, 5];
 // console.log(nizJedan);
@@ -278,7 +246,6 @@
 // }
 // console.log(nizJedan);
 // console.log(nizDva);
-
 // niz1 = [1, 2, 3, 4, 5];
 // niz2 = [];
 // console.log(niz1);
@@ -287,18 +254,15 @@
 //   niz2.push(niz1[i]);
 // }
 // niz1 = [];
-
 // niz1 = [1, 2, 3, 4, 5];
 // niz2 = [];
 // nizl = niz1.length;
-
 // for (let i = 0; i < nizl; i++) {
 //   el = niz1.pop();
 //   niz2.unshift(el);
 // }
 // console.log(niz1);
 // console.log(niz2);
-
 // a = [
 //   [1, 2, 1, 24],
 //   [8, 11, 9, 4],
@@ -306,11 +270,9 @@
 //   [7, 4, 28, 21],
 //   [3, 10, 26, 7],
 // ];
-
 // for (let i = 0; i < a.length; i++) {
 //   console.log(a[i]);
 // }
-
 // a = [
 //   [1, 2, 1, 24],
 //   [8, 11, 9, 4],
@@ -327,7 +289,6 @@
 //   }
 //   console.log(red);
 // }
-
 // a = [
 //   [0, 2, 1, 24],
 //   [8, 0, 9, 4],
@@ -340,7 +301,6 @@
 // for (let i = 0; i < a.length; i++) {
 //   console.log(a[i]);
 // }
-
 // a = [
 //   [1, 2, 1, 24],
 //   [8, 11, 9, 4],
@@ -360,7 +320,6 @@
 //   }
 //   console.log(red);
 // }
-
 // a = [
 //   [8, 11, 9, 4],
 //   [7, 0, 7, 21],
@@ -375,11 +334,9 @@
 //   }
 //   console.log(a[i]);
 // }
-
 // console.log(niz2.includes(niz1[0]));
 // console.log(niz2.includes(niz1[1]));
 // console.log(niz2.includes(niz1[2]));
-
 // niz1 = [1, 2, 3];
 // niz2 = [100, 2, 1, 10];
 // niz3 = [];
@@ -396,17 +353,14 @@
 //   }
 // }
 // console.log(niz3);
-
 // niz1 = [1, 2, 3];
 // niz2 = [2, 30, 1];
-
 // for (let i = 0; i < niz1.length; i++) {
 //   if (!niz2.includes(niz1[i])) {
 //     niz2.push(niz1[i]);
 //   }
 // }
 // console.log(niz2);
-
 // arr = ["b", 6, "a", "q", 7, 2];
 // arr1 = [];
 // for (let i = 0; i < arr.length; i++) {
@@ -417,7 +371,6 @@
 //   }
 // }
 // console.log(arr1);
-
 // niz = [
 //   ["a", 1, true],
 //   ["b", 2, false],
@@ -437,7 +390,6 @@
 //   }
 // }
 // console.log(nizResenje);
-
 // function capitaliseString() {
 //   let fox = "the quick brown fox";
 //   let foxarr = fox.split(" ");
@@ -449,20 +401,15 @@
 //   let foxarr = fox.join("");
 //   console.log(foxarr);
 // }
-
 // capitaliseString();
-
 // unos = prompt("unesite vase ime");
 // function ime(unos) {
 //   console.log("Vase ime je", unos);
 // }
-
 // ime(unos);
-
 // function twiceAsOld(dadYearsOld, sonYearsOld) {
 //   return Math.abs(dadYearsOld - 2 * sonYearsOld);
 // }
-
 // function broj(niz) {
 //   min1 = niz[0];
 //   min2 = niz[0];
@@ -477,7 +424,6 @@
 //   console.log("Drugi Najmanji je", min2);
 // }
 // broj([2, 3, 4, 7, 9]);
-
 // function nbroj(arr) {
 //   min1 = arr[0];
 //   arr = arr;
@@ -488,9 +434,7 @@
 //   }
 //   console.log("Najduzi broj je", min1);
 // }
-
 // nbroj([1, 21, 223, -5343]);
-
 // function nbroj(arr) {
 //   min1 = arr[0];
 //   for (el of arr) {
@@ -500,11 +444,8 @@
 //   }
 //   console.log("Najduzi broj je", min1);
 // }
-
 // nbroj([1, 21, 223, -5343]);
-
 // nule([3, 0, 24, 54, 0, 123, 0, 32, 63]);
-
 // function prosek(niz) {
 //   sum = 0;
 //   for (el of niz) {
@@ -512,9 +453,7 @@
 //   }
 //   console.log("prosek je", sum / niz.length);
 // }
-
 // prosek([3, 24, 54, 123, 32, 63]);
-
 // function poweroftwo(n) {
 //   pomArr = [];
 //   for (let i = 0; i <= n; i++) {
@@ -522,10 +461,8 @@
 //   }
 //   return pomArr;
 // }
-
 // rez = poweroftwo(3);
 // console.log(rez);
-
 // function reverseSeq(n) {
 //   niz = [];
 //   for (let i = n; i > 0; i--) {
@@ -533,10 +470,8 @@
 //   }
 //   return niz;
 // }
-
 // rez = reverseSeq(5);
 // console.log(rez);
-
 // function pillars(numpill, dist, width) {
 //   for (let i = 1; i < numpill; i++) {
 //     dist = dist + width;
@@ -544,10 +479,8 @@
 //   }
 //   return dist;
 // }
-
 // rez = pillars(2, 20, 25);
 // console.log(pillars(2, 20, 25));
-
 // function pillars(numpill, dist, width) {
 //   for (let i = 1; i < numpill; i++) {
 //     dist = dist + width;
@@ -555,16 +488,12 @@
 //   }
 //   return dist;
 // }
-
 // rez = pillars(2, 20, 25);
 // console.log(pillars(2, 20, 25));
-
 // function symmetricPoint(p, q) {
 //   return [2 * q[0] - p[0], 2 * q[1] - p[1]];
 // }
-
 // const iceBrickVolume = (r, x, y) => 2 * r * r * (x - y);
-
 // function stray(niz) {
 //   min1 = niz[0];
 //   for (el of niz) {
@@ -574,9 +503,7 @@
 //   }
 //   console.log(min1);
 // }
-
 // stray([1, 1, 1, 2]);
-
 // function stray(numbers) {
 //   num = 0;
 //   if (numbers[0] !== numbers[1] && numbers[0] !== numbers[2]) {
@@ -599,34 +526,26 @@
 // console.log(stray([1, 1, 2, 1, 1, 1, 1]));
 // console.log(stray([1, 1, 1, 1, 1, 1, 1, 17, 1, 1, 1]));
 // console.log(stray([1, 1, 1, 1, 1, 1, 1]));
-
 // shelly +amina = bff HAHAHAHHAHHAHAHAHHAHAH
-
 // function findLargestSumPair(arr) {
 //   let first, second;
-
 //   if (arr[0] > arr[1]) {
 //     first = arr[0];
 //     second = arr[1];
 //   } else {
 //     first = arr[1];
-
 //     second = arr[0];
 //   }
-
 //   for (let i = 2; i < arr.length; i++) {
 //     if (arr[i] > first) {
 //       second = first;
-
 //       first = arr[i];
 //     } else if (arr[i] > second && arr[i] != first) second = arr[i];
 //   }
 //   return first + second;
 // }
-
 // function sortiranje() {
 //   niz = [3, 44, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
-
 //   for (let i = 0; i < niz.length; i++) {
 //     for (let j = i + 1; j <= 0; j--) {
 //       if (niz[j] < niz[j - 1]) {
@@ -639,10 +558,8 @@
 //     }
 //   }
 // }
-
 // console.log(niz(niz.length - 1));
 // console.log(niz(niz.length - 2));
-
 // function nbYear(p0, percent, aug, p) {
 //   let n = 0;
 //   while (p0 < p) {
@@ -653,7 +570,6 @@
 // }
 //////////////////////////////
 //objekat je grupisanje nekih vrednosti
-
 // covek1 = {
 //   name: "amina",
 //   surname: "jusufovic",
@@ -663,15 +579,12 @@
 // };
 // console.log(Object.keys(covek1));
 // console.log(covek1.name);
-
 // var student = {
 //   name: "David Rey",
 //   sclass: "VI",
 //   rollno: 12,
 // };
-
 // console.log(Object.keys(student));
-
 // class Car {
 //   constructor(name, year) {
 //     this.name = name;
@@ -682,11 +595,9 @@
 //     return date.getFullYear() - this.year;
 //   }
 // }
-
 // c1 = new Car("audi", 2019);
 // console.log(c1);
 // console.log(c1.age());
-
 // class Macka {
 //   constructor(name, year, color, weight) {
 //     this.name = name;
@@ -695,16 +606,12 @@
 //     this.weight = weight;
 //   }
 // }
-
 // m1 = new Macka("Cici", 2022, "siva", "1kg");
 // console.log(m1);
-
 // m2 = new Macka("Kiki", 2018, "narandzasta", "3kg");
 // console.log(m2);
-
 // m3 = new Macka("Nini", 2015, "crna", "5kg");
 // console.log(m3);
-
 // o1 = {
 //   name: "Amina",
 //   surname: "Jusufovic",
@@ -715,10 +622,8 @@
 //   console.log(flatObj[i]);
 //   o2[flatObj[i][1]] = flatObj[i][0];
 // }
-
 // console.log(o1);
 // console.log(o2);
-
 // class Book {
 //   constructor(title = "", genre, author) {
 //     this.title = title;
@@ -728,7 +633,6 @@
 //     this.read_date = null;
 //   }
 // }
-
 // class BookLists {
 //   constructor() {
 //     this.bookShelf = [];
@@ -738,12 +642,10 @@
 //     this.currBook = null;
 //     this.lastBook = null;
 //   }
-
 //   add(book) {
 //     if (book instanceof Book) {
 //       debugger;
 //       this.bookShelf.push(book);
-
 //       for (let i = 0; i < this.bookShelf.length; i++) {
 //         if (!this.bookShelf[i].read) {
 //           this.currBook = this.bookShelf[i];
@@ -759,7 +661,6 @@
 //       console.log("Invalid argument type");
 //     }
 //   }
-
 //   finishCurrentBook() {
 //     for (let i = 0; i < this.bookShelf.length; i++) {
 //       const book = this.bookShelf[i];
@@ -776,44 +677,34 @@
 //     }
 //   }
 // }
-
 // b1 = new Book("HP1", "magic", "JKR");
 // b2 = new Book("HP2", "magic", "JKR");
 // b3 = new Book("HP3", "magic", "JKR");
 // b4 = new Book("HP4", "magic", "JKR");
 // b5 = new Book("HP5", "magic", "JKR");
-
 // bl = new BookLists();
-
 // bl.add(b1);
 // bl.add(b2);
 // bl.add(b3);
 // bl.add(b4);
 // bl.add(b5);
-
 // console.log(bl.lastBook);
 // console.log(bl.currBook);
 // console.log(bl.nextBook);
-
 // bl.finishCurrentBook();
-
 // console.log("=========================");
 // console.log(bl.lastBook);
 // console.log(bl.currBook);
 // console.log(bl.nextBook);
-
 // bl.finishCurrentBook();
 // bl.finishCurrentBook();
 // bl.finishCurrentBook();
 // bl.finishCurrentBook();
-
 // console.log("=========================");
 // console.log(bl.lastBook);
 // console.log(bl.currBook);
 // console.log(bl.nextBook);
-
 // console.log(bl.bookShelf);
-
 // class Macka
 // {
 //   constructor(tiredness, lonliness, hunger, happiness)
@@ -826,35 +717,27 @@
 //  igranje: function()
 // {
 //   hunger += 5;
-
 //   },
 // }
-
 // m1 = new Macka("");
 // console.log(m1);
-
 // m2 = new Macka();
 // console.log(m2);
-
 // o1 = {
 //   a: 1,
 //   b: 2,
 // };
 // console.log(object.entries(o1));
-
 // const data = {
 //   a: 1,
 //   b: 2,
 // };
-
 // console.log(data);
-
 // function withouth(data, prop) {
 //   delete data[prop];
 //   return data;
 // }
 // console.log(withouth({ a: 1, b: 2 }, "b"));
-
 // var library = [
 //   {
 //     author: "Bill Gates",
@@ -896,29 +779,99 @@
 //   }
 //   return rez;
 // }
-
 // console.log(godine(library));
-
 // function wash(callback) {
 //   setTimeout(function () {
 //     console.log("wash");
 //     callback();
 //   }, 3000);
 // }
-
 // function dry(callback) {
 //   setTimeout(function () {
 //     console.log("dry");
 //     callback();
 //   }, 3000);
 // }
-
 // function fold(callback) {
 //   setTimeout(function () {
 //     console.log("fold");
 //     callback();
 //   }, 3000);
 // }
-
 // doLaudry([wash, dry, fold]);
-``;
+
+// const numbers = [27, 28, 14, 6, 19, 55, 11];
+
+// function kvadrat(numbers, arg) {
+//   for (let i = 0; i < numbersy.length; i++) {
+//     numbers.array.forEach((element) => {
+//       numbers[i] * 2;
+//     });
+//     console.log(numbers);
+//   }
+// }
+// const numbers = [27, 28, 14, 6, 19, 55, 11];
+
+// var library = [
+//   {
+//     author: "Bill Gates",
+//     title: "The road ahead",
+//     readingStatus: true,
+//     age: 29,
+//   },
+//   {
+//     author: "Bill Gates",
+//     title: "The road ahead",
+//     readingStatus: true,
+//     age: 14,
+//   },
+//   {
+//     author: "Bill Gates",
+//     title: "The road ahead",
+//     readingStatus: true,
+//     age: 25,
+//   },
+//   {
+//     author: "Bill Gates",
+//     title: "The road ahead",
+//     readingStatus: true,
+//     age: 19,
+//   },
+//   {
+//     author: "Bill Gates",
+//     title: "The road ahead",
+//     readingStatus: true,
+//     age: 12,
+//   },
+// ];
+
+// const numbers = [45, 4, 9, 16, 25, 13, 38, 68, 61, 52, 29];
+// const evenNumbers = [];
+// numbers.forEach((el) => {
+//   if (el % 2 === 0) {
+//     evenNumbers.push(el);
+//   }
+// });
+// console.log(evenNumbers);
+
+// let str = "the quick brown fox";
+// const myArray = str.split(" ");
+// const newStr = "";
+// myArray.forEach((char, i) => {
+//   if (i % 2 === 0) {
+//     newStr += char.toLowerCase();
+//   } else {
+//     newStr += char.toUpperCase();
+//   }
+// });
+// console.log(myArray);
+
+data = [NaN, 0, 15, false, -22, "", undefined, 47, null];
+let counter = 0;
+data.forEach((element) => {
+  if (!element) {
+    counter++;
+  }
+});
+
+console.log(counter);
