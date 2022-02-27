c5b75db76c3454beeb6099b22f38c3dc04a7eb71
Amina <aminajusufovic4@gmail.com>
2022-02-28 00:01:06 +0100

    commit

--- js/vezba.js
+++ js/vezba.js
@@ -978,3 +978,19 @@
 // console.log(res2);
 
 // console.log(findMissing([0, 0, 0, 0, 0], [0, 0, 0, 0]),);
+
+const heros = [
+  { name: "Spider Man" },
+  { name: "Thor" },
+  { name: "Black Panther" },
+  { name: "Captain Marvel" },
+];
+
+const heros2 = heros.map((el, index) => {
+  return {
+    id: index,
+    hero: el.name,
+  };
+});
+
+console.log(heros2);
