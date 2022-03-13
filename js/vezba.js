e0e036132286c4376c7df2e202b4a40ea41de0f8
Amina <aminajusufovic4@gmail.com>
2022-03-13 17:08:01 +0100

    commit

--- js/vezba.js
+++ js/vezba.js
@@ -1202,9 +1202,29 @@
 // }
 // getData();
 
-async function getData() {
-  const response = await fetch("https://catfact.ninja/facts");
-  const resJson = await response.json();
-  console.log(resJson);
+// async function getData() {
+//   const response = await fetch("https://catfact.ninja/facts");
+//   const resJson = await response.json();
+//   console.log(resJson);
+// }
+// getData();
+
+var e = 10;
+function sum(a) {
+  return function (b) {
+    return function (c) {
+      return function (d) {
+        return a + b + c + d + e;
+      };
+    };
+  };
 }
-getData();
+
+f1 = sum(1);
+f2 = f1(2);
+f3 = f2(3);
+res = f3(4);
+
+console.log(res);
+
+console.log(sum(1)(2)(3)(4));
