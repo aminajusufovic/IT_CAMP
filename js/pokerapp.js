b80e2f1051658eb37b52e822ace4c60fdaaaa860
Amina <aminajusufovic4@gmail.com>
2022-03-22 18:53:19 +0100

    commit

--- js/pokerapp.js
+++ js/pokerapp.js
@@ -1,34 +1,34 @@
-window.onload = async function () {
-  const pokemonResourse = {};
-  const pokemonArr = [];
-  const pokemonInfo = [];
-  const getPokeList = async () => {
-    try {
-      const r = await fetch(
-        "https://pokeapi.co/api/v2/pokemon/?offset=10&limit=10"
-      );
-      const res = await r.json();
-      pokemonResourse = res;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+// window.onload = async function () {
+//   const pokemonResourse = {};
+//   const pokemonArr = [];
+//   const pokemonInfo = [];
+//   const getPokeList = async () => {
+//     try {
+//       const r = await fetch(
+//         "https://pokeapi.co/api/v2/pokemon/?offset=10&limit=10"
+//       );
+//       const res = await r.json();
+//       pokemonResourse = res;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-  const getPokemon = async (URL) => {
-    try {
-      const p = await fetch(URL);
-      const pokemon = await p.json();
-      pokemonInfo.push(pokemon);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//   const getPokemon = async (URL) => {
+//     try {
+//       const p = await fetch(URL);
+//       const pokemon = await p.json();
+//       pokemonInfo.push(pokemon);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-  await getPokeList();
-  pokemonArr = pokemonResourse.results.map((p) => {
-    return getPokemon(p.url);
-  });
+//   await getPokeList();
+//   pokemonArr = pokemonResourse.results.map((p) => {
+//     return getPokemon(p.url);
+//   });
 
-  await Promise.all(pokemonArr);
-  console.log(pokemonInfo);
-};
+//   await Promise.all(pokemonArr);
+//   console.log(pokemonInfo);
+// };
