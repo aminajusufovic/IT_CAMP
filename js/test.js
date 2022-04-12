8b85b13bddfda3f1434d452d4f9de1f10705bb31
Amina <aminajusufovic4@gmail.com>
2022-04-12 16:47:08 +0200

    comit

--- js/test.js
+++ js/test.js
@@ -8,15 +8,15 @@ function pattern(n) {
 
 pattern(10);
 
-function transpose(matrix) {
-  let arr = [];
-  for (let i = 0; i < matrix[0].slice().length; i++) {
-    let tempArr = [];
-    for (let j = 0; j < matrix.slice().length; j++) {
-      tempArr.push(matrix[j][i]);
-    }
-    arr.push(tempArr);
-  }
-  return arr;
-}
-transpose(1, 2, 3, 4, 5, 6);
+// function transpose(matrix) {
+//   let arr = [];
+//   for (let i = 0; i < matrix[0].slice().length; i++) {
+//     let tempArr = [];
+//     for (let j = 0; j < matrix.slice().length; j++) {
+//       tempArr.push(matrix[j][i]);
+//     }
+//     arr.push(tempArr);
+//   }
+//   return arr;
+// }
+// transpose(1, 2, 3, 4, 5, 6);
