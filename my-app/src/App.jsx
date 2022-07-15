d802bd5a90d20aa313027f1dfc19bbd2006cdaa6
Amina <aminajusufovic4@gmail.com>
2022-07-15 19:33:45 +0200

    commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./card";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
---
+++ my-app/src/card.jsx
@@ -0,0 +1,15 @@
+const Card = () => {
+  return (
+    <div className="card" style={{ border: "1px solid red" }}>
+      ;<h1>Ovo je card</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        perspiciatis recusandae vel nihil doloremque a dignissimos delectus,
+        deleniti dolore illum saepe minima rerum necessitatibus est totam ullam
+        adipisci. Earum, ab.
+      </p>
+    </div>
+  );
+};
+
+export default Card;
